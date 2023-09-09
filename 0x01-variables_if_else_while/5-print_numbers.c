dbff6c3c7362313b5eb5025cc75bd102375110d9
Eternityanane <eternityanane@gmail.com>
2023-09-08 20:21:09 -0700

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - where the task will execute first
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		if (i != 'e' && i != 'q')
+			putchar(i);
+	putchar('\n');
+	return (0);
+}
