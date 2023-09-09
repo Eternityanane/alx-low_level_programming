24987eb33efaa1f22beb5c4d532eefa5eab5461f
Eternityanane <eternityanane@gmail.com>
2023-09-08 21:16:55 -0700

    hexa

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - contains the initialization of the task
+ * Return: always 0
+ */
+int main(void)
+{
+
+	int i;
+	char j;
+
+	for (i = 0 ; i < 10 ; i++)
+		putchar(i + 'O');
+	for (j = 'a' ; j <= 'f' ; j++)
+		putchar(j);
+	putchar('\n');
+	return (0);
+}
