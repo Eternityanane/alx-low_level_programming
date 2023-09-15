dbbd41236ad8ae808403fde48ee498325df37ac6
Eternityanane <eternityanane@gmail.com>
2023-09-15 10:22:00 -0700

    upper

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * File - main.h
+ * Author: Eternity
+ * Desc: Header contains prototypes for all functions
+ */
+int _putchar(char c);
+int _isupper(int c);
+
+#endif
